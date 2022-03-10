27dc5e326664ebf4d05a0e49ec523eb48b98d5bf
Crivicsh <altosgamestv@gmail.com>
2022-03-10 23:03:44 +0300

    hs

--- js/index-equation.js
+++ js/index-equation.js
@@ -1,17 +1,31 @@
-link=document.getElementById('link');
-var link_text=document.location.origin + document.location.pathname;
-link.href=link_text;
-
+//заголовок
 document.getElementById("title").innerText='Chemine - Chemical equations online!';
-
-
-var reactions = {'hcl+mno2':[{"coefficient" : 4, "group":{"H":"", "Cl":""}, "physical-state":"(aq)", "color":"white"},"plus",{"coefficient" : "", "group":{"Mn": "", "O": 2}, "physical-state":"(s)", "color":"brown"},"arrow",{"coefficient" : 2, "group":{"H": 2, "O": ""}, "physical-state":"(l)", "color":"white"},"plus",{"coefficient" : "", "group":{"Cl": 2}, "physical-state":"(g)", "color":"green"},"plus",{"coefficient" : "", "group":{"Mn": "", "Cl": "2"}, "physical-state":"(aq)", "color":"pink"}],
-'h2+o2':[{"coefficient" : 2, "group":{"H":"2"}, "physical-state":"(g)", "color":"white"},'plus',{"coefficient" : '', "group":{"O":"2"}, "physical-state":"(g)", "color":"white"},"arrow",{"coefficient" : 2, "group":{"H": 2, "O": ""}, "physical-state":"(l)", "color":"white"}],
-
-};
+//проверка на отсутствие формы
+var link=document.location.search;
+if (link==''){
+    document.location='.';
+}
+//установка генеральной ссылки сайта
+link=document.getElementById('link');
+link.href='.';
+//установка введенных значений в input
 var paramsString = document.location.search;
 var searchParams = new URLSearchParams(paramsString).get('s');
 var input=document.getElementsByClassName('input')[0].value=searchParams;
-/*searchParams = searchParams.replace(/\s/g, '');
-searchParams = searchParams.toLowerCase().split('=')[0];
-console.log(searchParams);*/
+
+
+
+//подготовка строки для поиска в базе
+searchParams = searchParams.replace(/\s/g, '');
+searchParams = searchParams.toLowerCase();
+console.log(searchParams);
+
+//база, временная 
+var reactions = {
+    'hcl+mno2' : `
+    <span class="compound"><span class="coefficient">4</span><span class="space">&nbsp;</span><span class="group"><span class="element">H</span><span class="element">Cl</span><span class="oxidation-number-data-wrapper"><span class="oxidation-number-data" data-on="-I"></span></span></span><span class="space">&nbsp;</span><span class="physical-state">(aq)</span></span><span class="space"> </span><span class="plus">+</span><span class="space"> </span><span class="compound"><span class="group"><span class="element">Mn</span><span class="oxidation-number-data-wrapper"><span class="oxidation-number-data" data-on="IV"></span></span><span class="element">O</span><sub class="count">2</sub></span><span class="space">&nbsp;</span><span class="physical-state">(s)</span></span><span class="space"> </span><span class="arrow">→</span><span class="space"> </span><span class="compound"><span class="coefficient">2</span><span class="space">&nbsp;</span><span class="group"><span class="element">H</span><sub class="count">2</sub><span class="element">O</span></span><span class="space">&nbsp;</span><span class="physical-state">(l)</span></span><span class="space"> </span><span class="plus">+</span><span class="space"> </span><span class="compound"><span class="group"><span class="element">Cl</span><sub class="count">2</sub><span class="oxidation-number-data-wrapper"><span class="oxidation-number-data" data-on="0"></span></span></span><span class="space">&nbsp;</span><span class="physical-state">(g)</span></span><span class="space"> </span><span class="plus">+</span><span class="space"> </span><span class="compound"><span class="group"><span class="element">Mn</span><span class="oxidation-number-data-wrapper"><span class="oxidation-number-data" data-on="II"></span></span><span class="element">Cl</span><sub class="count">2</sub><span class="oxidation-number-data-wrapper"><span class="oxidation-number-data" data-on="-I"></span></span></span><span class="space">&nbsp;</span><span class="physical-state">(aq)</span></span><span class="space"> </span>`,
+
+}
+var reaction = document.createElement('div');
+reaction.innerHTML=reactions[searchParams];
+console.log(reaction.children);
--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-link=document.getElementById('link');
-var link_text=document.location.origin + document.location.pathname;
-link.href=link_text;
-
+//заголовок
 document.getElementById("title").innerText='Chemine - Chemical equations online!';
-
-
-var paramsString = document.location.search;
-var searchParams = new URLSearchParams(paramsString).get('s');
-var input=document.getElementsByClassName('input')[0].value=searchParams;
+//проверка на отсутствие формы
+var link=document.location.search;
+if (link!=''){
+    document.location='.';
+}
+//установка генеральной ссылки сайта
+link=document.getElementById('link');
+link.href='.';
