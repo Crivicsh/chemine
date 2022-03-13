44ae4a3f6f4311d25c90955af8131c4bf01af30e
Crivicsh <altosgamestv@gmail.com>
2022-03-13 03:33:20 +0300

    hs

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ if (link!=''){
 //установка генеральной ссылки сайта
 link=document.getElementById('link');
 link.href='.';
-
+console.log('брух');
 (async () => {
     var response = await fetch('https://chemequations.com/ru/');
     switch (response.status) {
