b321d62f1389c3b756835ae4c936a2ae7621219a
Crivicsh <altosgamestv@gmail.com>
2022-03-17 23:18:18 +0300

    hs

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ link.href='.';
 
 
 (async () => {
-    var response = await fetch('https://crivicsh.github.io/chemine/?s=hcl%2Bmno2&ref=input');
+    var response = await fetch('https://chemequations.com/ru/');
     switch (response.status) {
     // status "OK"
     case 200:
