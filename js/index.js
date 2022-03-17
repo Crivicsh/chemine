dbaca0c3b0c18be99d4e95fdeee674dad4a78c82
Crivicsh <altosgamestv@gmail.com>
2022-03-17 23:30:13 +0300

    hs

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ link.href='.';
 
 
 (async () => {
-    var response = await fetch('https://chemequations.com/ru/');
+    var response = await fetch('https://emequations.com/ru/');
     switch (response.status) {
     // status "OK"
     case 200:
